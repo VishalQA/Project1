51f7cfdd4d58489067236ad553f9dd2f0a914b38
VishalQA <vishal.mailbox1@gmail.com>
2020-05-11 22:19:41 +0530

    ok

---
+++ Project1/src/topics/Encapsulation.java
@@ -0,0 +1,36 @@
+package topics;
+
+public class Encapsulation {
+
+	    private int ssn;
+	    private String empName;
+	    private int empAge;
+
+	    //Getter and Setter methods
+	    public int getEmpSSN(){
+	        return ssn;
+	    }
+
+	    public String getEmpName(){
+	        return empName;
+	    }
+
+	    public int getEmpAge(){
+	        return empAge;
+	    }
+
+	    public void setEmpAge(int newValue){
+	        empAge = newValue;
+	    }
+
+	    public void setEmpName(String newValue){
+	        empName = newValue;
+	    }
+
+	    public void setEmpSSN(int newValue){
+	        ssn = newValue;
+	    }
+	}
+	
+
+	
